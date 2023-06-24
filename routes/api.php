86b04da69d7e52b3f0696fff57ad7d2a0bc7c5a3
Jovanka <jr20180105@student.fon.bg.ac.rs>
2023-06-24 22:35:27 +0200

    Napravljen kontroler za rad sa proizvodom i uvedene izmene

---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('login', function (Request $request) {
+    $user = User::where('email', $request->email)->first();
+    if (!$user || !Hash::check($request->password, $user->password)) {
+        return response("Failed login", 400);
+    }
+    return $user->createToken($user->id)->plainTextToken;
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', function (Request $request) {
+        $request->user()->currentAccessToken()->delete();
+        return response()->noContent();
+    });
+    Route::apiResource('products', ProductController::class);
+});
+ 
